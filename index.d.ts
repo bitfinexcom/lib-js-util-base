aaa7c827c02eae673ec1341b91aa769ef172a16c
dictor93 <dictor93@gmail.com>
2023-10-06 15:44:40 +0300

    Updated definition file

--- index.d.ts
+++ index.d.ts
@@ -8,3 +8,4 @@ export function pickBy(obj: Object, predicate: (val: any, key: string) => boolea
 export function camelize(str: string): string
 export function omit(obj: Object, keys: Array<string>): Object
 export function omitBy(obj: Object, predicate: (val: any, key: string) => boolean): Object
+export function get(obj: Object, path: string | Array<string | number>, defaultValue: any)
