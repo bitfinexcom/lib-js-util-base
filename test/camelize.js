a617d3aa66a46056a19874a15d3eaa724d13282c
vigan-abd <vigan.abd@gmail.com>
2023-10-10 17:48:03 +0200

    cleanup

--- test/camelize.js
+++ test/camelize.js
@@ -6,22 +6,22 @@ const assert = require('assert')
 const { camelize } = require('../index')
 
 describe('camelize', () => {
-  it('should convert strings to camelCase', function () {
+  it('should convert strings to camelCase', () => {
     assert.strictEqual(camelize('hello_world'), 'helloWorld')
     assert.strictEqual(camelize('Hello-world'), 'helloWorld')
     assert.strictEqual(camelize('hello world'), 'helloWorld')
   })
 
-  it('should handle single words', function () {
+  it('should handle single words', () => {
     assert.strictEqual(camelize('hello'), 'hello')
     assert.strictEqual(camelize('Hello'), 'hello')
   })
 
-  it('should handle empty strings', function () {
+  it('should handle empty strings', () => {
     assert.strictEqual(camelize(''), '')
   })
 
-  it('should handle strings with numbers', function () {
+  it('should handle strings with numbers', () => {
     assert.strictEqual(camelize('hello1_world2'), 'hello1World2')
   })
 })
