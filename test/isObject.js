505ffc862886eca37cb9c3f26fa9054595cb2f87
dictor93 <dictor93@gmail.com>
2023-10-31 22:53:51 +0200

    Made tests for isObject function

---
+++ test/isObject.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/* eslint-env mocha */
+
+const assert = require('assert')
+const { isObject } = require('../index')
+
+describe('isObject', () => {
+  it('should return true for plain object', () => {
+    const object = { a: 'foo' }
+    assert.ok(isObject(object))
+  })
+
+  it('should return true for function', () => {
+    const object = () => {}
+    assert.ok(isObject(object))
+  })
+
+  it('should return true for array', () => {
+    const object = []
+    assert.ok(isObject(object))
+  })
+
+  it('should return true for result of any constructor', () => {
+    assert.ok(isObject(new Set()))
+    assert.ok(isObject(new String()))
+    assert.ok(isObject(new Map()))
+    assert.ok(isObject(new Array()))
+  })
+
+  it('should return false for null and primitives', () => {
+    assert.ok(!isObject(null))
+    assert.ok(!isObject('foo'))
+    assert.ok(!isObject(123456))
+    assert.ok(!isObject(true))
+    assert.ok(!isObject(undefined))
+  })
+})
