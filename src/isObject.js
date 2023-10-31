8596c34743126371858545d69701de2e86c41684
dictor93 <dictor93@gmail.com>
2023-10-31 22:53:14 +0200

    Implemented isObject function

---
+++ src/isObject.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const isObject = (verifiable) => {
+  const type = typeof verifiable
+  if(verifiable == null) return false
+  return type === 'object' || type === 'function'
+}
+
+module.exports = isObject
