9cbc7f9b14cb4fad3624484c36e0bc880bc98ba9
dictor93 <dictor93@gmail.com>
2023-11-20 20:53:16 +0200

    Created function for strict compare two items

---
+++ src/util/strictEqual.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/**
+ * Strict compare two items
+ * @param {any} item 
+ * @param {any} another 
+ * @returns 
+ */
+const strictEqual = (item, another) => {
+  return item === another
+}
+
+module.exports = strictEqual
