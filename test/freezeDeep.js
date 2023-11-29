20aa7927c7582cb02e278e349064a8839335442c
vigan-abd <vigan.abd@gmail.com>
2023-11-29 13:47:03 +0100

    code refactor

--- test/freezeDeep.js
+++ test/freezeDeep.js
@@ -6,19 +6,19 @@ const assert = require('assert')
 const { freezeDeep } = require('../index')
 
 describe('freezeDeep', () => {
-  it('should freeze a simple object', function () {
+  it('should freeze a simple object', () => {
     const obj = { a: 1 }
     freezeDeep(obj)
     assert.throws(() => { obj.a = 2 }, TypeError)
   })
 
-  it('should deeply freeze an object', function () {
+  it('should deeply freeze an object', () => {
     const obj = { a: { b: { c: 1 } } }
     freezeDeep(obj)
     assert.throws(() => { obj.a.b.c = 2 }, TypeError)
   })
 
-  it('should not affect non-object properties', function () {
+  it('should not affect non-object properties', () => {
     const obj = { a: 1, b: 'test', c: true }
     freezeDeep(obj)
     assert.strictEqual(obj.a, 1)
@@ -26,12 +26,12 @@ describe('freezeDeep', () => {
     assert.strictEqual(obj.c, true)
   })
 
-  it('should handle objects that are already frozen', function () {
+  it('should handle objects that are already frozen', () => {
     const obj = Object.freeze({ a: 1 })
     assert.doesNotThrow(() => freezeDeep(obj))
   })
 
-  it('should handle null and undefined properties without errors', function () {
+  it('should handle null and undefined properties without errors', () => {
     const obj = { a: null, b: undefined }
     assert.doesNotThrow(() => freezeDeep(obj))
   })
