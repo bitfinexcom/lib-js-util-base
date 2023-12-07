86c0258123ec93f0830b3ed3094886b78e9c11d3
dictor93 <dictor93@gmail.com>
2023-12-07 04:48:07 +0200

    Test coverage for new pick functionality

--- test/pick.js
+++ test/pick.js
@@ -29,4 +29,21 @@ describe('pick', () => {
   it('should handle keys that are not strings', () => {
     assert.deepStrictEqual(pick({ 1: 'one', 2: 'two' }, ['1', '2']), { 1: 'one', 2: 'two' })
   })
+
+  it('should pick properties by few path parameters', () => {
+    assert.deepStrictEqual(pick({ a: 1, b: 2, c: 3, d: 4 }, ['a', 'b'], 'd'), { a: 1, b: 2, d: 4 })
+  })
+
+  it('should pick nested properties by string or array path', () => {
+    const src = { a: 1, b: { c: { d: 5, g: 2, h: 7 }, f: 6 }, e: 4 }
+    const expected = { a: 1, b: { c: { d: 5, h: 7 } } }
+
+    assert.deepStrictEqual(pick(src, ['a', 'b.c.d', 'b.c.h'], 'd'), expected)
+    assert.deepStrictEqual(pick(src, ['a', ['b', 'c', 'd'], ['b', 'c', 'h']], 'd'), expected)
+  })
+
+  it('should return empty object if path is not specified', () => {
+    const src = { a: 1, b: { c: { d: 5, g: 2, h: 7 }, f: 6 }, e: 4 }
+    assert.deepStrictEqual(pick(src, ''), {})
+  })
 })
