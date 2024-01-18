93c4cdcca2a61c6d8c13d1b4a310966b37ef0569
Jose Ramirez <jose.ramirez@bitfinex.com>
2024-01-18 05:02:45 -0400

    fixes

--- test/mapKeys.js
+++ test/mapKeys.js
@@ -5,13 +5,13 @@
 const assert = require('assert')
 const { mapKeys } = require('../index')
 
-describe('mapKeys', function () {
-  it('should map keys in `object` to a new object', function () {
+describe('mapKeys', () => {
+  it('should map keys in `object` to a new object', () => {
     const actual = mapKeys({ a: 1, b: 2 }, (_, k) => `test.${k}`)
     assert.deepStrictEqual(actual, { 'test.a': 1, 'test.b': 2 })
   })
 
-  it('should treat arrays like objects', function () {
+  it('should treat arrays like objects', () => {
     const actual = mapKeys([1, 2], String)
     assert.deepStrictEqual(actual, { 1: 1, 2: 2 })
   })
