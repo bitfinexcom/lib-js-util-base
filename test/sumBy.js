651f665f4f6e212ce08b4ee1bec85c80a245511d
dictor93 <dictor93@gmail.com>
2024-02-13 03:04:27 +0200

    Added tests for sum and sumBy

---
+++ test/sumBy.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const assert = require('assert')
+const { sumBy } = require('../index')
+
+describe('sum', () => {
+  it('should return a sum by iteratee function', () => {
+    assert.strictEqual(sumBy([{ foo: 1 }, { foo: 2 }, { foo: 3 }], (i) => i.foo), 6)
+  })
+
+  it('should return a sum by iteratee shortcut', () => {
+    assert.strictEqual(sumBy([{ foo: { bar: 4 } }, { foo: { bar: 5 } }, { foo: { bar: 6 } }], 'foo.bar'), 15)
+  })
+
+  it('should return 0 if iteratee argument is not a function or a string', () => {
+    assert.strictEqual(sumBy([{ foo: 1 }, { foo: 2 }, { foo: 3 }], {}), 0)
+    assert.strictEqual(sumBy([{ foo: 1 }, { foo: 2 }, { foo: 3 }], null), 0)
+  })
+})
