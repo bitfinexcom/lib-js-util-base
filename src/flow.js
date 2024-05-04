e5bf5cfa7537595b24ebaf6ee1ec0420e5ced654
dictor93 <dictor93@gmail.com>
2024-05-05 02:30:33 +0300

    Added flow spec

--- src/flow.js
+++ src/flow.js
@@ -1,3 +1,12 @@
+'use strict'
+
+/**
+ * Executes a series of functions in a pipeline, passing the result of each function as the argument to the next function.
+ * 
+ * @param {Array<Function>} fns - An array of functions to be executed in order.
+ * @returns {any} - The final result of the pipeline.
+ * @throws {Error} - Throws an error if any element in the array is not a function.
+ */
 const flow = (fns = []) => {
   if (fns.find(fn => typeof fn !== 'function')) throw new Error('All elements should be functions')
   return (arg) => fns.reduce((acc, fn) => fn(acc), arg)
