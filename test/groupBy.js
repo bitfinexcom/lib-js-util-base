75313605e308c34cf0b1f7072a6fb56f4ba1fb8a
paragmore <paragmore306@gmail.com>
2024-06-06 12:25:18 +0530

    Review

--- test/groupBy.js
+++ test/groupBy.js
@@ -8,85 +8,83 @@ const { groupBy } = require('../index')
 describe('groupBy', () => {
   it('should group items by string property key', () => {
     const items = [
-      { id: 1, category: 'fruit', name: 'apple' },
-      { id: 2, category: 'fruit', name: 'banana' },
-      { id: 3, category: 'vegetable', name: 'carrot' },
-      { id: 4, category: 'fruit', name: 'pear' },
-      { id: 5, category: 'vegetable', name: 'lettuce' }
+      { id: 1, category: 'fiat', name: 'USD' },
+      { id: 2, category: 'fiat', name: 'EUR' },
+      { id: 3, category: 'crypto', name: 'USDT' },
+      { id: 4, category: 'fiat', name: 'INR' },
+      { id: 5, category: 'crypto', name: 'EURT' }
     ]
 
     const groupedItems = groupBy(items, 'category')
 
     assert.deepStrictEqual(groupedItems, {
-      fruit: [
-        { id: 1, category: 'fruit', name: 'apple' },
-        { id: 2, category: 'fruit', name: 'banana' },
-        { id: 4, category: 'fruit', name: 'pear' }
+      fiat: [
+        { id: 1, category: 'fiat', name: 'USD' },
+        { id: 2, category: 'fiat', name: 'EUR' },
+        { id: 4, category: 'fiat', name: 'INR' }
       ],
-      vegetable: [
-        { id: 3, category: 'vegetable', name: 'carrot' },
-        { id: 5, category: 'vegetable', name: 'lettuce' }
+      crypto: [
+        { id: 3, category: 'crypto', name: 'USDT' },
+        { id: 5, category: 'crypto', name: 'EURT' }
       ]
     })
   })
 
   it('should group items by number property key', () => {
     const items = [
-      { id: 1, age: 25 },
-      { id: 2, age: 30 },
-      { id: 3, age: 25 },
-      { id: 4, age: 40 }
+      { id: 1, balance: 25 },
+      { id: 2, balance: 30 },
+      { id: 3, balance: 25 },
+      { id: 4, balance: 40 }
     ]
 
-    const groupedItems = groupBy(items, 'age')
+    const groupedItems = groupBy(items, 'balance')
 
     assert.deepStrictEqual(groupedItems, {
       25: [
-        { id: 1, age: 25 },
-        { id: 3, age: 25 }
+        { id: 1, balance: 25 },
+        { id: 3, balance: 25 }
       ],
-      30: [{ id: 2, age: 30 }],
-      40: [{ id: 4, age: 40 }]
+      30: [{ id: 2, balance: 30 }],
+      40: [{ id: 4, balance: 40 }]
     })
   })
 
   it('should group items by computed key using a function', () => {
     const items = [
-      { id: 1, category: 'fruit', name: 'apple' },
-      { id: 2, category: 'fruit', name: 'banana' },
-      { id: 3, category: 'vegetable', name: 'carrot' },
-      { id: 4, category: 'fruit', name: 'pear' },
-      { id: 5, category: 'vegetable', name: 'lettuce' }
+      { id: 1, category: 'fiat', name: 'USD' },
+      { id: 2, category: 'fiat', name: 'EUR' },
+      { id: 3, category: 'crypto', name: 'USDT' },
+      { id: 4, category: 'fiat', name: 'INR' },
+      { id: 5, category: 'crypto', name: 'EURT' }
     ]
 
     const groupedItems = groupBy(items, item => item.name.charAt(0))
 
     assert.deepStrictEqual(groupedItems, {
-      a: [{ id: 1, category: 'fruit', name: 'apple' }],
-      b: [{ id: 2, category: 'fruit', name: 'banana' }],
-      c: [{ id: 3, category: 'vegetable', name: 'carrot' }],
-      p: [{ id: 4, category: 'fruit', name: 'pear' }],
-      l: [{ id: 5, category: 'vegetable', name: 'lettuce' }]
+      U: [{ id: 1, category: 'fiat', name: 'USD' }, { id: 3, category: 'crypto', name: 'USDT' }],
+      E: [{ id: 2, category: 'fiat', name: 'EUR' }, { id: 5, category: 'crypto', name: 'EURT' }],
+      I: [{ id: 4, category: 'fiat', name: 'INR' }]
     })
   })
 
   it('should group items by function returning a number', () => {
     const items = [
-      { id: 1, age: 25 },
-      { id: 2, age: 30 },
-      { id: 3, age: 25 },
-      { id: 4, age: 40 }
+      { id: 1, balance: 25 },
+      { id: 2, balance: 30 },
+      { id: 3, balance: 25 },
+      { id: 4, balance: 40 }
     ]
 
-    const groupedItems = groupBy(items, item => Math.floor(item.age / 10))
+    const groupedItems = groupBy(items, item => Math.floor(item.balance / 10))
 
     assert.deepStrictEqual(groupedItems, {
       2: [
-        { id: 1, age: 25 },
-        { id: 3, age: 25 }
+        { id: 1, balance: 25 },
+        { id: 3, balance: 25 }
       ],
-      3: [{ id: 2, age: 30 }],
-      4: [{ id: 4, age: 40 }]
+      3: [{ id: 2, balance: 30 }],
+      4: [{ id: 4, balance: 40 }]
     })
   })
 
@@ -98,11 +96,11 @@ describe('groupBy', () => {
   })
 
   it('should handle array with one item', () => {
-    const items = [{ id: 1, category: 'fruit', name: 'apple' }]
+    const items = [{ id: 1, category: 'fiat', name: 'USD' }]
     const groupedItems = groupBy(items, 'category')
 
     assert.deepStrictEqual(groupedItems, {
-      fruit: [{ id: 1, category: 'fruit', name: 'apple' }]
+      fiat: [{ id: 1, category: 'fiat', name: 'USD' }]
     })
   })
 })
