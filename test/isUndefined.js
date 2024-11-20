20ae66d2a5f1c941bb0c130e6c1b2c51ab82a511
Sergio LR <sergiazow@gmail.com>
2024-11-20 20:29:32 +0100

    feat: add more test cases

--- test/isUndefined.js
+++ test/isUndefined.js
@@ -1,9 +1,8 @@
 'use strict'
 
 /* eslint-env mocha */
-
 const assert = require('assert')
-const { isUndefined } = require('../index')
+const isUndefined = require('../src/isUndefined')
 
 describe('isUndefined', () => {
   it('should return true for undefined', () => {
@@ -12,5 +11,29 @@ describe('isUndefined', () => {
 
   it('should return true for no param', () => {
     assert.ok(isUndefined())
+  })
+
+  it('should return false for empty object', () => {
+    assert.equal(isUndefined({}), false)
+  })
+
+  it('should return false for empty array', () => {
+    assert.equal(isUndefined([]), false)
+  })
+
+  it('should return false for null', () => {
+    assert.equal(isUndefined(null), false)
+  })
+
+  it('should return false for false', () => {
+    assert.equal(isUndefined(false), false)
+  })
+
+  it('should return false for empty string', () => {
+    assert.equal(isUndefined(''), false)
+  })
+
+  it('should return false for zero', () => {
+    assert.equal(isUndefined(0), false)
   })
 })
