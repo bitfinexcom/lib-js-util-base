80bbf22405974522b4a33f3d12336cc5f5fad81b
Sergio LR <sergiazow@gmail.com>
2025-03-03 11:12:31 +0100

    feat: add transformKeystoSnake + isPlainObject fix

---
+++ src/transformKeysToSnake.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const isPlainObject = require('./isPlainObject')
+const snakeCase = require('./snakeCase')
+
+/**
+ * Transform object keys to snake case
+ * e.g. { fooBar: 1 } => { foo_bar: 1 }
+ * @param {Object} obj
+ * @returns {Object}
+ */
+const transformKeysToSnakeCase = (obj) => {
+  if (!isPlainObject(obj)) {
+    return obj
+  }
+
+  return Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => [
+      snakeCase(k),
+      Array.isArray(v) ? v.map(transformKeysToSnakeCase) : transformKeysToSnakeCase(v)
+    ])
+  )
+}
+
+module.exports = transformKeysToSnakeCase
---
+++ test/transformKeystoSnake.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/* eslint-env mocha */
+
+const assert = require('assert')
+const { transformKeysToSnake } = require('../index')
+const { itEach } = require('mocha-it-each')
+
+describe('transformKeystoSnake', () => {
+  itEach('should return input when it is not an object', [new Date(), null, false], (input) => {
+    assert.equal(transformKeysToSnake(input), input)
+  })
+
+  it('should return input with snake case props recursively', () => {
+    const input = {
+      camelProp: 'true',
+      nestedProp: {
+        first_name: 'john',
+        lastName: 'doe',
+        tags: ['keepCamel']
+      },
+      someObjs: [
+        { someProp: 'value' },
+        { foo_bar: 'value', foo: 'value' }
+      ]
+    }
+    const output = {
+      camel_prop: 'true',
+      nested_prop: {
+        first_name: 'john',
+        last_name: 'doe',
+        tags: ['keepCamel']
+      },
+      some_objs: [
+        { some_prop: 'value' },
+        { foo_bar: 'value', foo: 'value' }
+      ]
+    }
+    assert.deepEqual(transformKeysToSnake(input), output)
+  })
+})
