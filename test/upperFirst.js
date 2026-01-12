148cfef3623d9c34b4a5135bda9c7bb96724a748
Abdelrahman Soliman <bebofit@aucegypt.edu>
2026-01-12 16:46:58 +0200

    handle same range for ip validation

--- test/upperFirst.js
+++ test/upperFirst.js
@@ -22,7 +22,15 @@ describe('upperFirst', () => {
     assert.strictEqual(upperFirst('f'), 'F')
   })
 
-  it('should handle non-string input', () => {
+  it('should handle number input', () => {
     assert.strictEqual(upperFirst(123), '123')
+  })
+
+  it('should handle object input', () => {
+    assert.strictEqual(upperFirst({}), '')
+  })
+
+  it('should handle array input', () => {
+    assert.strictEqual(upperFirst([]), '')
   })
 })
